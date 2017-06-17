2dd58b940f50beaa379e5e5eb8e4985aba846124
I Wayan Dharmana <wdharmana@gmail.com>
2017-06-17 16:56:51 +0800

    move api key configuration & remove back button

--- app/src/main/java/com/colonylabs/moviedb/utils/Constant.java
+++ app/src/main/java/com/colonylabs/moviedb/utils/Constant.java
@@ -1,12 +1,14 @@
 package com.colonylabs.moviedb.utils;
 
+import com.colonylabs.moviedb.BuildConfig;
+
 /**
  * Created by dharmana on 6/17/17.
  */
 
 public class Constant {
 
-    public static String MOVIE_API_KEY = "<API_KEY_HERE>";
+    public static String MOVIE_API_KEY = BuildConfig.MOVIE_API_KEY;
     public static String MOVIE_API_BASE_URL = "https://api.themoviedb.org/3/";
     public static String MOVIE_IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w185";
 
